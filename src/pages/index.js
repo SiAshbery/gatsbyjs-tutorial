e0212c25198a7d401f32488677f16a0764afa85c
Simon Ashbery <simon.ashbery@gmail.com>
2020-04-24 15:02:29 +0100

    Changed subheader

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby";
 export default () => (
   <div style={{ color: `purple` }}>
     <h1>Ello DAVE</h1>
-    <p>WHAT WORLD</p>
+    <p>WOLOLO</p>
     <Link to="/about/">ABOUT</Link>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
